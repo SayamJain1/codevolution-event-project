24491eb15960a74b64894268fbe4caf0fe7f8328
Sanyam <sayamjain99300@gmail.com>
2023-11-01 15:23:18 +0530

    initial commit

---
+++ src/components/framework-rotation.tsx
@@ -0,0 +1,32 @@
+import { assets } from "@/utils/asset-utils";
+import { type Framework, frameworks } from "@/utils/framework-utils";
+import { cn } from "@/utils/tailwind-utils";
+import Image from "next/image";
+
+export const FrameworRotation = ({
+  currentFramework,
+}: {
+  currentFramework: Framework;
+}) => {
+  return (
+    <div className="w-[80px] h-[80px] mx-2 -mt-2 align-middle inline-flex relative">
+      {frameworks.map((name, index) => (
+        <Image
+          key={name}
+          alt="logo"
+          src={assets[name]}
+          width={80}
+          height={80}
+          className={cn(
+            "w-full h-full object-contain object-center absolute top-0 left-0 transition-all duration-300",
+            currentFramework === name
+              ? "opacity-100 transform-none"
+              : index > frameworks.indexOf(currentFramework as Framework)
+              ? "opacity-0 -translate-y-2"
+              : "opacity-0 translate-y-2"
+          )}
+        />
+      ))}
+    </div>
+  );
+};
